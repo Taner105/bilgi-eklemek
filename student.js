b83a8e24780c7b1f1b0e6454c95bf5e9ab69f4c2
Taner105 <taneraydogan59@gmail.com>
2021-11-17 18:41:43 +0300

    .

---
+++ student.js
@@ -0,0 +1,19 @@
+// class Student1{
+//     constructorStudent(imageUrl, studentName, studentPath){
+//         console.log(this);
+//     }
+// }
+function Student(imageUrl, studentName, studentPath){
+    // console.log(this);
+    // console.log(self);
+    this.imageUrl = imageUrl;
+    this.studentName = studentName;
+    this.studentPath = studentPath;
+    // console.log(this);
+}
+
+const std = new Student("url", "Ryan", "Fullstack");
+const std2 = new Student("url", "Daniel", "AWS");
+
+// console.log(std);
+// console.log(std2);
